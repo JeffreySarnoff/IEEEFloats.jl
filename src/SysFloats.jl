4f7f488f00f2053a6834f17a1e2ba923742d85c9
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-12-11 23:06:21 -0500

    Update SysFloats.jl

--- src/SysFloats.jl
+++ src/SysFloats.jl
@@ -1,8 +1,11 @@
 module SysFloats
 
 export bitwidth, signbit, sign, precision, exponent, significand,
-       exponent_max, exponent_min, exponent_field_max
-
+       exponent_max, exponent_min, exponent_field_max,
+       get_sign_field, get_exponent_field, get_signficand_field,
+       get_sign_and_exponent_fields, get_exponent_and_significand_fields,
+       set_sign_field, set_exponent_field, set_signficand_field,
+       set_sign_and_exponent_fields, set_exponent_and_significand_fields
 
 import Base.Math: precision, significand_bits, exponent_bits
 
