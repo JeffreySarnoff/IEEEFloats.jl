b6dd91cf7444894d2ce602c4dff23a96299e2cc2
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-03-06 18:34:07 -0500

    Update corresponding_sizes.jl

--- src/corresponding_sizes.jl
+++ src/corresponding_sizes.jl
@@ -1,26 +1,3 @@
-
-"""
-    Float128
-
-This placeholder is used to reference related constants.
-""" Float128
-
-"""
-    Float8
-
-This placeholder is used to reference related constants.
-""" Float8
-
-"""
-    Binary8
-
-This placeholder is used to reference related constants.
-""" Binary8
-
-struct Float128 <: AbstractFloat end
-struct Float8   <: AbstractFloat end
-struct Binary8  <: AbstractFloat end
-
 """
     ieeefloat(T)
 
