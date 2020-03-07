b2cf9a1d6ef7fbd65f100bf61df87580f9e92d1d
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-03-06 19:04:30 -0500

    Update use_fields.jl

--- src/use_fields.jl
+++ src/use_fields.jl
@@ -1,49 +1,49 @@
 
 # field[s] offset (shift by)
 
-@inline sign_field_offset(::Type{T}) where T<:IEEEFloat = bitwidth(T) - one(signed(T))
-@inline exponent_field_offset(::Type{T}) where T<:IEEEFloat = sign_field_offset(T) - exponent_bits(T)
-@inline significand_field_offset(::Type{T}) where T<:IEEEFloat = zero(signed(T))
-@inline sign_and_exponent_fields_offset(::Type{T}) where T<:IEEEFloat = exponent_field_offset(T)
-@inline exponent_and_significand_fields_offset(::Type{T}) where T<:IEEEFloat = significand_field_offset(T)
+@inline sign_field_offset(::Type{T}) where T = bitwidth(T) - one(signed(T))
+@inline exponent_field_offset(::Type{T}) where T = sign_field_offset(T) - exponent_bits(T)
+@inline significand_field_offset(::Type{T}) where T = zero(signed(T))
+@inline sign_and_exponent_fields_offset(::Type{T}) where T = exponent_field_offset(T)
+@inline exponent_and_significand_fields_offset(::Type{T}) where T = significand_field_offset(T)
 
 # field[s] filter and mask
 
-@inline sign_field_filter(::Type{T}) where T<:IEEEFloat = ~(zero(unsigned(T)) >>> 1
-@inline sign_and_exponent_fields_filter(::Type{T}) where T<:IEEEFloat = ~(zero(unsigned(T)) >>> (exponent_bits(T) + 1)
-@inline exponent_field_filter(::Type{T}) where T<:IEEEFloat = sign_and_exponent_fields_filter(T) | sign_field_mask(T)
-@inline significand_field_filter(::Type{T}) where T<:IEEEFloat = ~sign_and_exponent_fields_filter(T)
-@inline exponent_and_significand_fields_filter(::Type{T}) where T<:IEEEFloat = ~(sign_field_filter(T))
+@inline sign_field_filter(::Type{T}) where T = ~(zero(unsigned(T)) >>> 1
+@inline sign_and_exponent_fields_filter(::Type{T}) where T = ~(zero(unsigned(T)) >>> (exponent_bits(T) + 1)
+@inline exponent_field_filter(::Type{T}) where T = sign_and_exponent_fields_filter(T) | sign_field_mask(T)
+@inline significand_field_filter(::Type{T}) where T = ~sign_and_exponent_fields_filter(T)
+@inline exponent_and_significand_fields_filter(::Type{T}) where T = ~(sign_field_filter(T))
 
-@inline sign_field_mask(::Type{T}) where T<:IEEEFloat = ~sign_field_filter(T)
-@inline sign_and_exponent_fields_mask(::Type{T}) where T<:IEEEFloat = ~sign_and_exponent_fields_filter(T)
-@inline exponent_field_mask(::Type{T}) where T<:IEEEFloat = ~exponent_field_filter(T)
-@inline significand_field_mask(::Type{T}) where T<:IEEEFloat = ~sign_and_exponent_fields_mask(T)
-@inline exponent_and_significand_fields_mask(::Type{T}) where T<:IEEEFloat = ~exponent_and_significand_fields_mask(T)
+@inline sign_field_mask(::Type{T}) where T = ~sign_field_filter(T)
+@inline sign_and_exponent_fields_mask(::Type{T}) where T = ~sign_and_exponent_fields_filter(T)
+@inline exponent_field_mask(::Type{T}) where T = ~exponent_field_filter(T)
+@inline significand_field_mask(::Type{T}) where T = ~sign_and_exponent_fields_mask(T)
+@inline exponent_and_significand_fields_mask(::Type{T}) where T = ~exponent_and_significand_fields_mask(T)
 
-@inline sign_field_mask_lsbs(::Type{T}) where T<:IEEEFloat = sign_field_mask(T) >> sign_field_offset(T)
-@inline sign_and_exponent_fields_mask_lsbs(::Type{T}) where T<:IEEEFloat = sign_and_exponent_fields_mask(T) >> exponent_field_offset(T)
-@inline exponent_field_mask_lsbs(::Type{T}) where T<:IEEEFloat = exponent_field_mask(T) >> exponent_field_offset(T)
-@inline significand_field_mask_lsbs(::Type{T}) where T<:IEEEFloat = significand_fields_mask(T) >> significand_field_offset(T)
-@inline exponent_and_significand_fields_mask_lsbs(::Type{T}) where T<:IEEEFloat = exponent_and_significand_fields_mask(T) >> significand_field_offset(T)
+@inline sign_field_mask_lsbs(::Type{T}) where T = sign_field_mask(T) >> sign_field_offset(T)
+@inline sign_and_exponent_fields_mask_lsbs(::Type{T}) where T = sign_and_exponent_fields_mask(T) >> exponent_field_offset(T)
+@inline exponent_field_mask_lsbs(::Type{T}) where T = exponent_field_mask(T) >> exponent_field_offset(T)
+@inline significand_field_mask_lsbs(::Type{T}) where T = significand_fields_mask(T) >> significand_field_offset(T)
+@inline exponent_and_significand_fields_mask_lsbs(::Type{T}) where T = exponent_and_significand_fields_mask(T) >> significand_field_offset(T)
 
 # isolate the field[s] from other bits and yield the field value, as Unsigned bits in place
        
-@inline sign_field(x::T) where T<:IEEEFloat = convert(Unsigned, x) & sign_field_mask(T)
-@inline exponent_field(x::T) where T<:IEEEFloat = convert(Unsigned, x) & exponent_field_mask(T)
-@inline significand_field(x::T) where T<:IEEEFloat = convert(Unsigned, x) & significand_field_mask(T)
-@inline sign_and_exponent_fields(x::T) where T<:IEEEFloat = convert(Unsigned, x) & sign_and_exponent_field_mask(T)
-@inline exponent_and_significand_fields(x::T) where T<:IEEEFloat = convert(Unsigned, x) & sign_field_filter(T)
-@inline sign_and_significand_fields(x::T) where T<:IEEEFloat = convert(Unsigned, x) & exponent_field_mask(T)
+@inline sign_field(x::T) where T = convert(Unsigned, x) & sign_field_mask(T)
+@inline exponent_field(x::T) where T = convert(Unsigned, x) & exponent_field_mask(T)
+@inline significand_field(x::T) where T = convert(Unsigned, x) & significand_field_mask(T)
+@inline sign_and_exponent_fields(x::T) where T = convert(Unsigned, x) & sign_and_exponent_field_mask(T)
+@inline exponent_and_significand_fields(x::T) where T = convert(Unsigned, x) & sign_field_filter(T)
+@inline sign_and_significand_fields(x::T) where T = convert(Unsigned, x) & exponent_field_mask(T)
 
 # clear the field[s] and yield the value, as Unsigned bits in place
 
-@inline clear_sign_field(x::T) where T<:IEEEFloat = convert(Unsigned, x) & sign_field_filter(T)
-@inline clear_exponent_field(x::T) where T<:IEEEFloat = convert(Unsigned, x) & exponent_field_filter(T)
-@inline clear_significand_field(x::T) where T<:IEEEFloat = convert(Unsigned, x) & significand_field_filter(T)
-@inline clear_sign_and_exponent_fields(x::T) where T<:IEEEFloat = convert(Unsigned, x) & sign_and_exponent_field_filter(T)
-@inline clear_exponent_and_significand_fields(x::T) where T<:IEEEFloat = convert(Unsigned, x) & exponent_and_significand_fields_filter(T)
-@inline clear_sign_and_significand_fields(x::T) where T<:IEEEFloat = convert(Unsigned, x) & exponent_field_mask(T)
+@inline clear_sign_field(x::T) where T = convert(Unsigned, x) & sign_field_filter(T)
+@inline clear_exponent_field(x::T) where T = convert(Unsigned, x) & exponent_field_filter(T)
+@inline clear_significand_field(x::T) where T = convert(Unsigned, x) & significand_field_filter(T)
+@inline clear_sign_and_exponent_fields(x::T) where T = convert(Unsigned, x) & sign_and_exponent_field_filter(T)
+@inline clear_exponent_and_significand_fields(x::T) where T = convert(Unsigned, x) & exponent_and_significand_fields_filter(T)
+@inline clear_sign_and_significand_fields(x::T) where T = convert(Unsigned, x) & exponent_field_mask(T)
 
 # fetch the field[s] into the low order bits of an Unsigned
 
