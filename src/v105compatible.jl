fb71c6d5717b6c5fd1904c542ab7401092e8127d
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-03-07 00:47:33 -0500

    Update v105compatible.jl

--- src/v105compatible.jl
+++ src/v105compatible.jl
@@ -1,9 +1,9 @@
 if isdefined(Base, :exponent_bits)
     using Base: exponent_bits
 else
-    exponent_bits(::Type{Float64}) = 52
-    exponent_bits(::Type{Float32}) = 23
-    exponent_bits(::Type{Float16}) = 10
+    exponent_bits(::Type{Float64}) = 11
+    exponent_bits(::Type{Float32}) =  8
+    exponent_bits(::Type{Float16}) =  5
 end
 if isdefined(Base, :significand_bits)
     using Base: significand_bits
