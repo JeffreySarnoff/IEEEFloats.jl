75b70335963ed22a98cdb1a5af4688965a968891
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-03-06 23:54:06 -0500

    Update use_fields.jl

--- src/use_fields.jl
+++ src/use_fields.jl
@@ -46,7 +46,7 @@ exponent_significand_offset(::Type{T}) where T = significand_offset(T)
 
 # field filters
 
-sign_filter(::Type{T}) where T = ~(zero(unsigned(T)) >>> 1
+sign_filter(::Type{T}) where T = ~zero(unsigned(T)) >>> 1
 exponent_filter(::Type{T}) where T = sign_exponent_filter(T) | sign_mask(T)
 significand_filter(::Type{T}) where T = ~sign_exponent_filter(T)
 sign_exponent_filter(::Type{T}) where T = ~(zero(unsigned(T)) >>> (exponent_bits(T) + 1)
